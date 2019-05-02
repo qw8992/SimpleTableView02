1ee69d8981fa69abf8486caca00b38349f922607
D7703_29 <d7703_29@d7703-29ui-imac.local>
2019-05-02 14:37:06 +0900

    Initial Commit

---
+++ SimpleTableView02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTableView02
+//
+//  Created by D7703_29 on 2019. 5. 2..
+//  Copyright © 2019년 D7703_29. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
