008686408a4cfde535ddd08f1e0744d9f5dea2cf
D7703_29 <d7703_29@d7703-29ui-imac.local>
2019-05-02 15:32:46 +0900

    commit

--- SimpleTableView02/ViewController.swift
+++ SimpleTableView02/ViewController.swift
@@ -8,12 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
+    
+    @IBOutlet weak var myTableView: UITableView!
+    var animals = ["Cat", "Dog" , "Cow", "Pig", "Horse", "Whale"]
+    var year = ["3", "5", "10", "2", "6", "3"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        myTableView.dataSource = self
+        
     }
+    
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return animals.count
+    }
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "RE", for: indexPath)
+        let row = indexPath.row
+        
+        cell.textLabel?.text = animals[row]
+        
+        cell.detailTextLabel?.text = year[row]
+        
+        return cell
+    }
+    
 
 
 }
